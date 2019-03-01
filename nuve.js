8052b5a1869d6283b4893f61317a6914ff8db3f6
teevi <webstd.design@gmail.com>
2019-03-02 01:56:02 +0200

    false true

--- nuve.js
+++ nuve.js
@@ -8,6 +8,17 @@ config.nuve.dataBaseURL = `${nuve_database_url}/nuvedb`; // default value: 'loca
 config.rabbit = {};
 config.rabbit.host = process.env.HOST_RABBITMQ || "172.15.0.20"; //default value: 'localhost'
 
-console.log(`Server is listening on port ${nuve_database_url} nuve ${config.nuve.dataBaseURL} rabbit ${config.rabbit.host}`);
+config.erizoController = {};
+
+const ackuaria_stat = !!(process.env.ACKUARIA);
+config.erizoController.report = {
+//const ackuaria_stat = !!(process.env.ACKUARIA);
+    session_events: ackuaria_stat,             // Session level events -- default value: false
+    connection_events: false, //ackuaria_stat,  // Connection (ICE) level events -- default value: false
+    rtcp_stats: false //ackuaria_stat                   // RTCP stats -- default value: false
+};
+
+
+console.log(`Server is listening on port ${nuve_database_url} nuve ${config.nuve.dataBaseURL} rabbit ${config.rabbit.host} session_events: ${config.erizoController.report.session_events}`);
 
 
