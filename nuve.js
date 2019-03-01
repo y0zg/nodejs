e31589790e122912f53a24e21fcb16dc906e1ff0
teevi <webstd.design@gmail.com>
2019-03-01 22:45:26 +0200

    convert var to string

--- nuve.js
+++ nuve.js
@@ -1,6 +1,13 @@
 var config = {}
-const nuve_database_url = process.env.HOST_MONGO || XXX;
+const nuve_database_url = process.env.HOST_MONGO || "172.15.0.10";
 config.nuve = {};
 //config.nuve.dataBaseURL = "$nuve_database_url/nuvedb"; // default value: 'localhost/nuvedb'
 config.nuve.dataBaseURL = nuve_database_url+"/nuvedb"; // default value: 'localhost/nuvedb'
-console.log(`Server is listening on port ${nuve_database_url} ${config.nuve.dataBaseURL}`);
+
+const rabbit_host = process.env.HOST_RABBITMQ || "172.15.0.20";
+config.rabbit = {};
+config.rabbit.host = rabbit_host; //default value: 'localhost'
+
+console.log(`Server is listening on port ${nuve_database_url} nuve ${config.nuve.dataBaseURL} rabbit ${config.rabbit.host}`);
+
+
