ae03e53eceed31de508e8494015b5f11c8783861
teevi <webstd.design@gmail.com>
2019-03-02 01:20:47 +0200

    correct way to pass vars into string

--- nuve.js
+++ nuve.js
@@ -2,11 +2,11 @@ var config = {}
 const nuve_database_url = process.env.HOST_MONGO || "172.15.0.10";
 config.nuve = {};
 //config.nuve.dataBaseURL = "$nuve_database_url/nuvedb"; // default value: 'localhost/nuvedb'
-config.nuve.dataBaseURL = nuve_database_url+"/nuvedb"; // default value: 'localhost/nuvedb'
+config.nuve.dataBaseURL = `${nuve_database_url}/nuvedb`; // default value: 'localhost/nuvedb'
 
-const rabbit_host = process.env.HOST_RABBITMQ || "172.15.0.20";
+//const rabbit_host = process.env.HOST_RABBITMQ || "172.15.0.20";
 config.rabbit = {};
-config.rabbit.host = rabbit_host; //default value: 'localhost'
+config.rabbit.host = process.env.HOST_RABBITMQ || "172.15.0.20"; //default value: 'localhost'
 
 console.log(`Server is listening on port ${nuve_database_url} nuve ${config.nuve.dataBaseURL} rabbit ${config.rabbit.host}`);
 
